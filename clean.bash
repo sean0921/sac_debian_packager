9dfc72ed4d799f80ced6033c9574b22d5b25fd69
Sean Ho <sean.li.shin.ho@gmail.com>
2020-06-26 20:33:00 +0800

    initial commit

---
+++ clean.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -eux
+
+printf "\033[1m+ Cleaning previous build...\033[0m\n"
+test -d pkgroot && rm -r pkgroot
+test -e *.deb && rm *.deb
+test -e sac-*/ && rm -r sac-*/
+printf "\033[1;32m    - Done!\033[0m\n"
