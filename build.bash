c27740906841759dbe43207297cf0e733bb5d499
Sean Ho <sean.li.shin.ho@gmail.com>
2021-05-14 17:10:28 +0800

    fix(build.bash): specific signed type for char
    In most cases, char is unsigned on ARM (for performance reasons) and signed on other platforms.
    https://stackoverflow.com/questions/20576300/is-char-signed-or-unsigned-by-default-on-ios
    
    So if our system/toolchain defaults to an unsigned char type. That means when getopt() returns -1,
    it gets converted to 255 and stored in t.
    https://stackoverflow.com/questions/17070958/c-why-does-getopt-return-255-on-linux
    
    The workaround is specific "char" type to signed by default for SAC is
    developed and delivered mostly in Intel-x86 environment (that assumes
    char is signed)

--- build.bash
+++ build.bash
@@ -63,6 +63,9 @@ case "$1" in
         MKDIR_ARGUMENT="-pv"
         AUTORECONF_ARUMENT="-fiv"
         ;;
+    --arm64)
+        ARCH="arm64"
+        ;;
 esac
 
 #################################### Function for phrasing OS detections
@@ -144,7 +147,7 @@ patch $QUIET -p1 < ../0002-correct-name-of-autoreconf-file-configure.ac.patch
 patch $QUIET -p1 < ../0003-correct-automake-variable-syntax.patch
 rm $RM_ARGUMENT bin/sac-config bin/sacinit.csh bin/sacinit.sh
 autoreconf $AUTORECONF_ARUMENT
-./configure CFLAGS="-fcommon" --prefix="/opt/sac" --enable-readline $QUIET
+./configure CFLAGS="-fcommon -fsigned-char" --prefix="/opt/sac" --enable-readline $QUIET
 printf "\033[1;32m    - Done!\033[0m\n"
 
 #################################### Building SAC...
